207c78625498c0054159aadac21436f825f119c1
Tayeb Sedraia <tayebsedraia@MacBook-Pro-de-Tayeb.local>
2017-06-20 12:18:16 +0200

    map

--- Ionic/POCPacifica/src/pages/map/map.ts
+++ Ionic/POCPacifica/src/pages/map/map.ts
@@ -1,10 +1,19 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 
 import { AlertController, App, FabContainer, ItemSliding, List, ModalController } from 'ionic-angular';
-import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native';
+
 import { Geolocation } from '@ionic-native/geolocation';
 import { Platform } from 'ionic-angular';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+ GoogleMaps,
+ GoogleMap,
+ GoogleMapsEvent,
+ LatLng,
+ CameraPosition,
+ MarkerOptions,
+ Marker
+} from '@ionic-native/google-maps';
 
 /**
  * Generated class for the Map page.
@@ -21,43 +30,53 @@ export class Map {
 
    map: GoogleMap;
  
-    constructor(public navCtrl: NavController, public platform: Platform) {
-        platform.ready().then(() => {
-            this.loadMap();
-        });
-    }
- 
-    loadMap(){
- 
-        let location = new GoogleMapsLatLng(-34.9290,138.6010);
- 
-        this.map = new GoogleMap('map', {
-          'backgroundColor': 'white',
-          'controls': {
-            'compass': true,
-            'myLocationButton': true,
-            'indoorPicker': true,
-            'zoom': true
-          },
-          'gestures': {
-            'scroll': true,
-            'tilt': true,
-            'rotate': true,
-            'zoom': true
-          },
-          'camera': {
-            'latLng': location,
-            'tilt': 30,
-            'zoom': 15,
-            'bearing': 50
-          }
-        });
- 
-        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
-            console.log('Map is go!');
-        });
+    constructor(private googleMaps: GoogleMaps) {}
 
-        this.map.setDebuggable(true);
- 
-    }
+    ngAfterViewInit() {
+ this.loadMap();
 }
+ 
+loadMap() {
+ // make sure to create following structure in your view.html file
+ // and add a height (for example 100%) to it, else the map won't be visible
+ // <ion-content>
+ //  <div #map id="map" style="height:100%;"></div>
+ // </ion-content>
+
+ // create a new map by passing HTMLElement
+ let element: HTMLElement = document.getElementById('map');
+
+ let map: GoogleMap = this.googleMaps.create(element);
+
+ // listen to MAP_READY event
+ // You must wait for this event to fire before adding something to the map or modifying it in anyway
+ map.one(GoogleMapsEvent.MAP_READY).then(
+   () => {
+     console.log('Map is ready!');
+     // Now you can add elements to the map like the marker
+   }
+ );
+
+ // create LatLng object
+ let ionic: LatLng = new LatLng(43.0741904,-89.3809802);
+
+ // create CameraPosition
+ let position: CameraPosition = {
+   target: ionic,
+   zoom: 18,
+   tilt: 30
+ };
+
+ // move the map's camera to position
+ map.moveCamera(position);
+
+ // create new marker
+ let markerOptions: MarkerOptions = {
+   position: ionic,
+   title: 'Ionic'
+ };
+
+ }
+
+}
+ 
